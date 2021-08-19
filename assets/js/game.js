1b7243b51c97db58a566c96c8aa5fed589f163c1
Graciela Morales <gracielamorales@Gracielas-MacBook-Pro.local>
2021-08-18 20:01:15 -0700

    adding more to js code

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,15 @@
-window.alert("This is on alert! javaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+// Note the lack of quotation marks around playerName
+console.log(playerName);
+console.log("This logs as a string, good for leaving yourself a message");
+// this will do match and log 20
+console.log(10+10);
+// what is this?
+console.log("Our robot's mame is " + playerName);
+
+// this creates a function named "fight"
+function fight () {
+    window.alert("The fight has begun!");
+}
+
+// fight();
