1ffa7959ff9f1285e3a5774596ab7b73062d05eb
Graciela Morales <gracielamorales@Gracielas-MacBook-Pro.local>
2021-08-21 23:56:29 -0700

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -15,58 +15,58 @@ console.log(enemyNames[3]);
 //fight function (now with parameter for enemy's name)
 var fight = function(enemyName) {
   while (playerHealth > 0 && enemyHealth > 0) {
-      //ask player if they'd like to fight or run
-      var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+    //ask player if they'd like to fight or run
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
-      // if player picks "skip" confirm and then stop the loop
-      if (promptFight === "skip" || promptFight === "SKIP") {
-        // confirm player wants to skip
-        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+    // if player picks "skip" confirm and then stop the loop
+    if (promptFight === "skip" || promptFight === "SKIP") {
+      // confirm player wants to skip
+      var confirmSkip = window.confirm("Are you sure you'd like to quit?");
     
-        // if yes (true), leave fight
-        if (confirmSkip) {
-            window.alert (playerName + " has decided to skip this fight. Goodbye!");
-            // subtract money from playerMoney for skipping
-            playerMoney = playerMoney - 10;
-            console.log("playerMoney", playerMoney);
-            break;
-        }
+      // if yes (true), leave fight
+      if (confirmSkip) {
+        window.alert (playerName + " has decided to skip this fight. Goodbye!");
+        // subtract money from playerMoney for skipping
+        playerMoney = playerMoney - 10;
+        console.log("playerMoney", playerMoney);
+        break;
       }
-
-      //remove enemy's health by subtracting the amount set in the playerAttack variable
-      enemyHealth = enemyHealth - playerAttack;
-      console.log(
-        playerName + " attacked " + enemyName + " . " + enemyName + " now has " + enemyHealth + " health remaining. "
-      );
-
-      //check enemy's health
-      if (enemyHealth <= 0) {
-         window.alert(enemyName + " has died!");
-
-         //award player money for winning
-         playerMoney = playerMoney + 20;
- 
-         //leave while () loop since enemy is dead
-         break;
-        } else {
-          window.alert(enemyName + " still has " + enemyHealth + " health left.");
-        }
-
-      // remove player's health by subracting the amount set in the enemyAttack variable
-      playerHealth = playerHealth - enemyAttack;
-      console.log(
-          enemyName + " attacked " + playerName + " . " + playerName + " now has " + playerHealth + " health remaining."
-       );
-
-       //check player's health
-       if (playerHealth <= 0) {
-         window.alert(playerName + " has died!");
-         //leave while() loop if player is dead
-         break;
-        } else {
-            window.alert(playerName + " still has " + playerHealth + " health left.");
-        }
     }
+
+    //remove enemy's health by subtracting the amount set in the playerAttack variable
+    enemyHealth = enemyHealth - playerAttack;
+    console.log(
+      playerName + " attacked " + enemyName + " . " + enemyName + " now has " + enemyHealth + " health remaining. "
+    );
+
+    //check enemy's health
+    if (enemyHealth <= 0) {
+      window.alert(enemyName + " has died!");
+
+      //award player money for winning
+      playerMoney = playerMoney + 20;
+ 
+      //leave while () loop since enemy is dead
+      break;
+    } else {
+      window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+
+    // remove player's health by subracting the amount set in the enemyAttack variable
+    playerHealth = playerHealth - enemyAttack;
+    console.log(
+      enemyName + " attacked " + playerName + " . " + playerName + " now has " + playerHealth + " health remaining."
+    );
+
+    //check player's health
+    if (playerHealth <= 0) {
+      window.alert(playerName + " has died!");
+      //leave while() loop if player is dead
+      break;
+    } else {
+      window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
+  }
 }; 
 
 // function to start a new game
@@ -80,25 +80,23 @@ var startGame = function() {
  for (var i = 0; i < enemyNames.length; i++) {
   // if player is still alive, keep fighting
   if (playerHealth > 0) {
-      //let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
-      window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
+    //let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
+    window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
 
-      //pick new enemy to fight based on the index of the enemyNames array
-      var pickedEnemyName = enemyNames[i];
+    //pick new enemy to fight based on the index of the enemyNames array
+    var pickedEnemyName = enemyNames[i];
 
-      //reset enemyHealth before starting new fight
-      enemyHealth = 50;
+    //reset enemyHealth before starting new fight
+    enemyHealth = 50;
 
-      //use debugger to pause script from running and check what's gooing on at that moment in the code
-      // debugger;
-
-      //pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
-      fight(pickedEnemyName);
+    //pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+    fight(pickedEnemyName);
+      
   }
   // if player isn't alive, stop the game
   else {
-      window.alert('You have lost your robot in the battle! Game Over!');
-      break;
+    window.alert('You have lost your robot in the battle! Game Over!');
+    break;
   }
  }
 
@@ -125,7 +123,6 @@ var endGame = function() {
   } else {
     window.alert("Thank you for playing Robot Gladiators! Come back sonn!");
   }
-  
 };
 
 // play again
